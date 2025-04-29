e02c06a19e121bf52f46f4f890c7a0e7c6f723ca
unknown <za5232208@gmail.com>
2025-04-29 11:57:51 +0500

    NewCommienablecors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,17 +7,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// // Middleware to parse JSON
-// const corsOptions = {
-//   origin: [
-//     "https://infatica-dashboard-frontend.vercel.app",
-//     "http://localhost:5173",
-//   ],
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// };
+// Middleware to parse JSON
+const corsOptions = {
+  origin: [
+    "https://infatica-dashboard-frontend.vercel.app",
+    "http://localhost:5173",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
