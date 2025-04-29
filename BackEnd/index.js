84d45ba93f8416c99bb1bfc00ab6c9edc6acba2e
unknown <za5232208@gmail.com>
2025-04-29 11:55:13 +0500

    NewComiADdCors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import trafficRoutes from "./routes/allapisroute.js"; // Import your new router
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware to parse JSON
+// // Middleware to parse JSON
+// const corsOptions = {
+//   origin: [
+//     "https://infatica-dashboard-frontend.vercel.app",
+//     "http://localhost:5173",
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
+
+// app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Routes
